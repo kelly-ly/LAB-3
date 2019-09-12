4f33272f88e2effe2d62101b6ad598871fa7c0bc
Kelly LUO <kelly.luoying@foxmail.com>
2019-09-12 22:54:53 +0200

    Include the dataset wiki_graph in the package.

--- R/Lab3_group11.R
+++ R/Lab3_group11.R
@@ -51,3 +51,25 @@ dijkstra = function(df, init_nod){
   }
   return(result)
 }
+
+require(datasets)
+
+
+#' WIKI GRAPH
+#'
+#' A data.frame contains the edges of the graph (from v1 to v2) with the weight of the edge (w).
+#' diamonds.
+#'
+#' @format A data frame with three variables:
+#' \describe{
+#'   \item{v1}{start of the edges}
+#'   \item{v2}{end of the edges}
+#'   \item{w}{weight of the edges}
+#'   
+#' }
+#' @source \url{https://en.wikipedia.org/wiki/Graph_(discrete_mathematics)}
+
+wiki_graph <- data.frame(v1=c(1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,6), 
+                         v2=c(2,3,6,1,3,4,1,2,4,6,2,3,5,4,6,1,3,5), 
+                         w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9))
+save(wiki_graph, file = "wiki_graph.RData")
